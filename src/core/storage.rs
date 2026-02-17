40b92f2bd36d27de1717becb7af7b454f531258d
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-17 19:02:33 +0700

    refactor(storage): Simplify LogStore trait by removing StorageError enum definition

--- src/core/storage.rs
+++ src/core/storage.rs
@@ -1,13 +1,6 @@
-pub mod storage;
+use crate::core::StorageError;
 
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum StorageError {
-    #[error("IO failure: {0}")]
-    Io(#[from] std::io::Error),
-    #[error("Segment full")]
-    SegmentFull,
-    #[error("Checksum mismatch at offset {0}")]
-    Corruption(u64)
+pub trait LogStore: Send + Sync {
+    fn append(&mut self, data: &[u8]) -> Result<u64, StorageError>;
+    fn sync(&self) -> Result<(), StorageError>;
 }
